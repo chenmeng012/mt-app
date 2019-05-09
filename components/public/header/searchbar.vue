aa1caecdfe2f901bccb9070b360359b54615a799
chenmeng <chenmeng@cailian.net>
2019-05-09 15:25:46 +0800

    header 布局

---
+++ components/public/header/searchbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="search-panel">
+    <el-row class="m-header-searchbar">
+      <el-col :span="3" class="left">
+        <img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png" alt="美团" />
+      </el-col>
+      <el-col :span="15" class="center">
+        <div class="wrapper">
+          <el-input v-model="search" @focus="focus" @blur="blur" @input="input" placeholder="搜素商家或地点" />
+          <button class="el-button el-button--primary"><i class="el-icon-search"/></button>
+          <dl class="hotPlace" v-if="isHotPlace">
+            <dt>热门搜索</dt>
+            <dd v-for="(item, index) in hotPlace" :key="index">{{item}}</dd>
+          </dl>
+          <dl class="searchList" v-if="isSearchList">
+            <dd v-for="(item, index) in searchList" :key="index">{{item}}</dd>
+          </dl>
+        </div>
+        <p class="suggset">
+          <a href="#">故宫博物馆</a>
+          <a href="#">故宫博物馆</a>
+          <a href="#">故宫博物馆</a>
+          <a href="#">故宫博物馆</a>
+          <a href="#">故宫博物馆</a>
+        </p>
+        <ul class="nav">
+          <li>
+            <nuxt-link to="/" class="takeout">美团外卖</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="movie">猫眼电影</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="hotel">美团酒店</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="apartment">民宿/公寓</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="apartment">商家入驻</nuxt-link>
+          </li>
+        </ul>
+      </el-col>
+      <el-col :span="6" class="right">
+        <ul class="security">
+          <li><i class="refund" /><p class="text">随时退</p></li>
+          <li><i class="single" /><p class="text">不满意免单</p></li>
+          <li><i class="overdue" /><p class="text">过期退</p></li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data() {
+    return {
+      search: '',
+      isFocus: false,
+      hotPlace: ['火锅'],
+      searchList: ['火锅'],
+    }
+  },
+  computed: {
+    isHotPlace() {
+      return !this.search && this.isFocus
+    },
+    isSearchList() {
+      return this.search && this.isFocus
+    }
+  },
+  methods: {
+    focus() {
+      this.isFocus = true;
+    },
+    blur() {
+      setTimeout(() => {
+        this.isFocus = false;
+      }, 200);
+    },
+    input(e) {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
