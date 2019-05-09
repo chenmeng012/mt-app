3c76f089434bd6eeb87eeacef48d07e21b62aa74
chenmeng <chenmeng@cailian.net>
2019-05-09 11:33:36 +0800

    引入css img 等文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ module.exports = {
   */
   css: [
     'element-ui/lib/theme-chalk/index.css',
-    'element-ui/lib/theme-chalk/reset.css'
+    'element-ui/lib/theme-chalk/reset.css',
+    '~assets/css/main.css'
   ],
 
   /*
